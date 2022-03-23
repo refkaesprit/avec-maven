8826a4a648b460cfe78407fe96dd42fd1e0be569
arwa.lemaalem <arwa.lemaalem@medianet.com.tn>
2022-03-23 13:12:30 +0100

    commit validation 2

--- src/main/java/tn/esprit/Main.java
+++ src/main/java/tn/esprit/Main.java
@@ -3,7 +3,7 @@ package tn.esprit;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Equipe de validation");
+		System.out.println("Equipe de validation ++");
 	}
 
 }
