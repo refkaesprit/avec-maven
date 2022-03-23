1576a8f337c003e7df557e1bc058b27b0fe65043
arwa.lemaalem <arwa.lemaalem@medianet.com.tn>
2022-03-23 13:59:33 +0100

    add new line in main

--- src/main/java/tn/esprit/Main.java
+++ src/main/java/tn/esprit/Main.java
@@ -3,13 +3,8 @@ package tn.esprit;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Equipe de validation ++");
-=======
-
-		// TODO Auto-generated method stub
-		System.out.println("this is Test team 1");
->>>>>>> refs/remotes/origin/main
+		System.out.println("Equipe de validation/Test/Preprode");
+		System.out.println("Equipe de validation");
 	}
 
 }
