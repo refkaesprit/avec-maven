80556adec2b1f2d4ae5863b60e6fc29761704871
arwa.lemaalem <arwa.lemaalem@medianet.com.tn>
2022-03-26 08:49:54 +0100

    fix conflicts

--- src/main/java/tn/esprit/Test.java
+++ src/main/java/tn/esprit/Test.java
@@ -1,16 +1,10 @@
 package tn.esprit;
 
 public class Test {
-<<<<<<< HEAD
 	public void PrintingGenre() {
 		System.out.println("Liste des genres : ");
 		for (Genre genre : Genre.values()) {
 			System.out.print("   " +genre+ "   ");
 		}
-=======
-	public void PrintingGenre(){
-	for (Genre genre : Genre.values()) {
-		System.out.println();
->>>>>>> refs/heads/TEST
-	}
+}
 }
